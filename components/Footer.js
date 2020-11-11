b9970c8bfb55f6a264a669c4df222d3cce9ff5c8
clewis890 <clewis890@gmail.com>
2020-11-11 00:33:12 -0300

    minor changes

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,14 @@ export default function Footer() {
     return (
         <>
         <footer className={styles.footer}>
-            <Link href="/"><a className={styles.footerlink}>Inicio</a></Link>
-            <Link href="/registry"><a className={styles.footerlink}>Lista de Casamento</a></Link>
-            <Link href="/guest-book"><a className={styles.footerlink}>Livro de Visitas</a></Link>
-            <Link href="/photo-album"><a className={styles.footerlink}>Album de Fotos</a></Link>
-            <Link href="/rsvp"><a className={styles.footerlink}>Confirmar Presença</a></Link>
+            <Link href="/"><a className={styles.footerlink}><p>Inicio</p></a></Link>
+            <Link href="/registry"><a className={styles.footerlink}><p>Lista de Casamento</p></a></Link>
+            <Link href="/guest-book"><a className={styles.footerlink}><p>Livro de Visitas</p></a></Link>
+            <Link href="/photo-album"><a className={styles.footerlink}><p>Album de Fotos</p></a></Link>
+            <Link href="/rsvp"><a className={styles.footerlink}><p>Confirmar Presença</p></a></Link>
+            <div className={styles.copyright}>
+                <p>Copyright Clark Lewis Web Development 2020 &copy;</p>
+            </div>
         </footer>
         </>
       )
