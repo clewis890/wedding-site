1a35d6d359c462e9b7dfddb4aa1590923861c692
clewis890 <clewis890@gmail.com>
2020-11-12 11:58:18 -0300

    added favicon

--- pages/photo-album.js
+++ pages/photo-album.js
@@ -12,7 +12,10 @@ import DisqusComments from '.././components/Blog'
             <Head>
                 <title>Album de Fotos - Antonia e Clark</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
 
         <main className={styles.main}>
@@ -42,7 +45,9 @@ import DisqusComments from '.././components/Blog'
         <div className={styles.albumpic}>
           <Image src="/images/antoniaclark7.jpg" alt="Antonia and Clark" width="250" height="350" />
         </div>
-       
+        {/* <div className={styles.albumpic}>
+          <Image src="/images/antoniaclark8.jpeg" alt="Antonia and Clark" width="400" height="450" />
+        </div> */}
         <div className={styles.albumpic}>
           <Image src="/images/antoniaclark9.jpeg" alt="Antonia and Clark" width="250" height="350" />
         </div>
--- pages/registry.js
+++ pages/registry.js
@@ -11,9 +11,12 @@ export default function Registry() {
         <>
         <div className={styles.container}>
             <Head>
-                <title>Lista de Casamento</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Lista de Casamento - Antonia e Clark</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </Head>
 
         <main className={styles.main}>
